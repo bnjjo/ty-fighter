15ba452a06cb208a3883a841064eed090366a05f
bsuaklo <bsukalo@outlook.com>
2026-01-01 23:23:42 +0100

    update texts

--- server/texts.js
+++ server/texts.js
@@ -1,7 +1,7 @@
 const entries = [
-  "The quick brown fox jumps over the lazy dog",
-  "Programming is the art of telling another human what one wants the computer to do",
-  "In the middle of difficulty lies opportunity",
+  "The quick brown fox jumps over the lazy dog.",
+  "Programming is the art of telling another human what one wants the computer to do.",
+  "In the middle of difficulty lies opportunity.",
 ];
 const length = entries.length;
 
