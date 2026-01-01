74ac9d0c60cdce486c13463b6aecbffe0ec8b5a6
bsuaklo <bsukalo@outlook.com>
2026-01-01 17:41:26 +0100

    initialize react project for client and set up theme + font

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      Hello, World!
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const THEME = 'rose-pine';
+document.documentElement.setAttribute('data-theme', THEME);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
