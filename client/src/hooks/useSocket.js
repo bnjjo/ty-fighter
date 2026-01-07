c153feab9a7502af7dd4d8448f765676c97676bd
bsuaklo <bsukalo@outlook.com>
2026-01-07 08:18:20 +0100

    clean up useSocket hook

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://ty-fighter.onrender.com';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const useSocket = () => {
   const [socket, setSocket] = useState(null);
